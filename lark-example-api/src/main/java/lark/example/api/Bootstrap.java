9a222259c317e0a8adcec770dc016049f87487fb
andy.yuan <goodidea2002@gmail.com>
2020-12-17 15:38:29 +0800

    CI

--- lark-example-api/src/main/java/lark/example/api/Bootstrap.java
+++ lark-example-api/src/main/java/lark/example/api/Bootstrap.java
@@ -2,10 +2,8 @@ package lark.example.api;
 
 import lark.api.boot.ApiApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class} )
+@SpringBootApplication
 public class Bootstrap {
     public static void main(String[] args) {
         ApiApplication app = new ApiApplication(Bootstrap.class);
